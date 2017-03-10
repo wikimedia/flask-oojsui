3f008685d7ec39b171901fc3bb0e67dd4436b6b7
halfak <aaron.halfaker@gmail.com>
2017-03-10 16:14:30 -0600

    Increments version to 0.0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def requirements(fname):
 
 setup(
     name='flask-oojsui',
-    version="0.0.2",
+    version="0.0.3",
     author='Aaron Halfaker',
     author_email='aaron.halfaker@gmail.com',
     packages=find_packages(),
